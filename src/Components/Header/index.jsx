6162e1eda23dc4937b7b42cb0a7c0790650038bc
DatVoVN <dat246642@gmail.com>
2024-08-27 02:28:54 +0700

    Header

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,5 +1,11 @@
 import { Link } from "react-router-dom"
 import Logo from "../../assets/images/47553-bacola-logo.png"
+import CountryDropdown from "../CountryDropdown"
+import Button from "@mui/material/Button"
+import { IoBagOutline } from "react-icons/io5"
+import { FiUser } from "react-icons/fi"
+import SearchBox from "./SearchBox"
+import Navigation from "./Navigation"
 const Header = () => {
   return (
     <>
@@ -12,7 +18,7 @@ const Header = () => {
             </p>
           </div>
         </div>
-        <div className="header">
+        <header className="header">
           <div className="container">
             <div className="row">
               <div className="logoWrapper d-flex align-items-center col-sm-2">
@@ -20,9 +26,30 @@ const Header = () => {
                   <img src={Logo} alt="Logo" />
                 </Link>
               </div>
+              <div className="col-sm-10 d-flex align-items-center part2">
+                <CountryDropdown />
+                <SearchBox />
+                <div className="part3 d-flex align-items-center ml-auto">
+                  <Button className="circle mr-3">
+                    <FiUser />
+                  </Button>
+                  <div className="ml-auto cartTab d-flex align-items-center">
+                    <span className="price">$3.29</span>
+                    <div className="position-relative ml-2">
+                      <Button className="circle ml-2">
+                        <IoBagOutline />
+                      </Button>
+                      <span className="count d-flex align-items-center justify-content-center">
+                        1
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
+        </header>
+        <Navigation />
       </div>
     </>
   )
